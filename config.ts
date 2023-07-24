5470b9f133736a4dc1a091e56458f5bfc7ad2ece
CoronaBringer <65749353+CoronaBringer@users.noreply.github.com>
2023-07-24 06:14:18 +0200

    chore: bump version to v0.5.0

--- config.ts
+++ config.ts
@@ -2,6 +2,6 @@
 export default {
   hostPassword: process.env.HOST_PASSWORD || "1234",
   maxDelay: parseInt(process.env.MAX_DELAY!) || 5000,
-  clientVersionRequirements: "0.4.x",
-  clientRecommendedVersion: "0.4.3"
+  clientVersionRequirements: "0.5.x",
+  clientRecommendedVersion: "0.5.0"
 }
